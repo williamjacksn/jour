31dd28cedcc9362067aba3c1041110a0a67a5f06
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-03-08 11:45:19 -0600

    Fix types

--- jour/models/settings.py
+++ jour/models/settings.py
@@ -5,7 +5,7 @@ if typing.TYPE_CHECKING:
     import fort
 
 
-def _get(db: 'fort.SqliteDatabase', setting_id: str):
+def _get(db: 'fort.SQLiteDatabase', setting_id: str):
     sql = '''
         select setting_value
         from settings
@@ -17,7 +17,7 @@ def _get(db: 'fort.SqliteDatabase', setting_id: str):
     return db.q_val(sql, params)
 
 
-def _set(db: 'fort.SqliteDatabase', setting_id: str, setting_value: str):
+def _set(db: 'fort.SQLiteDatabase', setting_id: str, setting_value: str):
     sql = '''
         insert into settings (setting_id, setting_value) values (:setting_id, :setting_value)
         on conflict (setting_id) do update set setting_value = excluded.setting_value
@@ -29,7 +29,7 @@ def _set(db: 'fort.SqliteDatabase', setting_id: str, setting_value: str):
     db.u(sql, params)
 
 
-def get_enc(db: 'fort.SqliteDatabase', setting_id: str):
+def get_enc(db: 'fort.SQLiteDatabase', setting_id: str):
     val = _get(db, setting_id)
     if val:
         f = cryptography.fernet.Fernet(secret_key(db))
@@ -37,7 +37,7 @@ def get_enc(db: 'fort.SqliteDatabase', setting_id: str):
     return ''
 
 
-def get_str(db: 'fort.SqliteDatabase', setting_id: str):
+def get_str(db: 'fort.SQLiteDatabase', setting_id: str):
     val = _get(db, setting_id)
     if val:
         return str(val)
@@ -46,16 +46,16 @@ def get_str(db: 'fort.SqliteDatabase', setting_id: str):
 
 def secret_key(db: 'fort.SQLiteDatabase'):
     setting_id = 'flask/secret-key'
-    secret_key = _get(db, setting_id)
-    if secret_key is None:
+    existing_key = _get(db, setting_id)
+    if existing_key is None:
         _set(db, setting_id, cryptography.fernet.Fernet.generate_key().decode())
     return _get(db, setting_id)
 
 
-def set_enc(db: 'fort.SqliteDatabase', setting_id: str, setting_value: str):
+def set_enc(db: 'fort.SQLiteDatabase', setting_id: str, setting_value: str):
     f = cryptography.fernet.Fernet(secret_key(db))
-    _set(db, setting_id, f.encrypt(setting_value.encode()))
+    _set(db, setting_id, f.encrypt(setting_value.encode()).decode())
 
 
-def set_str(db: 'fort.SqliteDatabase', setting_id: str, setting_value: str):
+def set_str(db: 'fort.SQLiteDatabase', setting_id: str, setting_value: str):
     _set(db, setting_id, setting_value)
