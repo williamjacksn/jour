7bfe55b10dbc834e8304078eef39cbc6ee76b5a4
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-03-08 11:48:47 -0600

    Add a database table to store journal data

--- jour/models/__init__.py
+++ jour/models/__init__.py
@@ -6,11 +6,17 @@ if typing.TYPE_CHECKING:
     import fort
 
 
-def init(db: 'fort.SqliteDatabase'):
-    sql = '''
+def init(db: 'fort.SQLiteDatabase'):
+    db.u('''
         create table if not exists settings (
             setting_id text primary key,
             setting_value text
         )
-    '''
-    db.u(sql)
+    ''')
+    db.u('''
+        create table if not exists journals (
+            journal_id uuid primary key,
+            journal_date date,
+            journal_data text
+        )
+    ''')
