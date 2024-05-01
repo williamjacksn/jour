91512a707af3517f5304637158b6d8056e3553bd
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-05-01 16:45:37 -0500

    Handle html with multiple root elements

--- jour/filters.py
+++ jour/filters.py
@@ -6,7 +6,7 @@ import markupsafe
 def md(t: str) -> markupsafe.Markup:
     """Take markdown-formatted text as input and render into HTML."""
     _md = markdown.Markdown()
-    doc = lxml.html.fragment_fromstring(_md.convert(t))
+    doc = lxml.html.fragment_fromstring(_md.convert(t), create_parent='div')
     for el in doc.xpath(f'//blockquote'):
         el.classes.update(('border-3', 'border-start', 'ps-2'))
     result = lxml.html.tostring(doc, encoding='unicode')
