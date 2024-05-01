05f8d68a281adc8f0f155af3ba325b4fd0f4aaae
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-05-01 16:22:46 -0500

    Filter for adding CSS classes to tags

--- jour/filters.py
+++ jour/filters.py
@@ -1,5 +1,14 @@
+import lxml
 import markdown
 import markupsafe
+
+
+def add_css_class_to_tag(html: str, tag: str, css_class: str) -> markupsafe.Markup:
+    doc = lxml.html.fragment_fromstring(html)
+    for el in doc.xpath(f'//{tag}'):
+        tag.classes.add(css_class)
+    result = lxml.html.tostring(doc, encoding='unicode')
+    return markupsafe.Markup(result)
 
 
 def md(t: str) -> markupsafe.Markup:
