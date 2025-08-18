76cf5736902908012e31e71709d9d34f1f7ce80b
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-08-18 13:59:18 -0500

    Add CLI script to display OpenID settings

---
+++ cli.py
@@ -0,0 +1,14 @@
+import jour.app
+import jour.models
+
+
+def main():
+    db = jour.app._get_db()
+    settings = jour.models.settings.Settings(db)
+    print("openid_client_id:", settings.openid_client_id)
+    print("openid_client_secret:", settings.openid_client_secret)
+    print('openid_discovery_document:', settings.openid_discovery_document)
+
+
+if __name__ == "__main__":
+    main()
